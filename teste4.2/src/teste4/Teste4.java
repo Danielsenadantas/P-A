4b06a2dfda23bd3c3a3abfccf6f60535454cddf7
Dansena <107227427+Danielsenadantas@users.noreply.github.com>
2026-03-25 12:34:57 -0300

    Add files via upload

---
+++ teste4.2/src/teste4/Teste4.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teste4;
+
+/**
+ *
+ * @author Admin
+ */
+public class Teste4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 1, b = -5, c = 6, soma = 0, d = 0, soma1 = 0;
+    
+          d = Math.sqrt( b**2 - 4 * a * c); 
+                 soma = (b- + d) / 2 * a;
+                 soma1 = (b- - d) / 2 * a;
+                 System.out.println("x1 >" + soma);
+                 System.out.println("x2 >" + soma1);
+                
+                
+        
+        
+        // TODO code application logic here
+    }
+    
+}
