a0e880a8ee210ec89393dad18948948449440156
Dansena <107227427+Danielsenadantas@users.noreply.github.com>
2026-04-15 12:31:24 -0300

    Add files via upload

---
+++ Estudos/src/estudos/Estudos.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package estudos;
+
+/**
+ *
+ * @author Adminv
+ */
+public class Estudos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int A;
+        double B;
+        long C;    
+        A = 10;
+        System.out.println("Variavel " + A);
+        // TODO code application logic here
+            
+    }
+    
+}
