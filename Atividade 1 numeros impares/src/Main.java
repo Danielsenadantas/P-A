9aeff53ebb5554f3b2cb4d0c46190cd4813685d0
Dansena <107227427+Danielsenadantas@users.noreply.github.com>
2026-05-25 23:39:55 -0300

    Add files via upload

---
+++ Atividade 1 numeros impares/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 75;
+        while (i < 197) {
+            if (i % 2 != 0) {
+                System.out.println("esse numero eh impar " + i);
+            }
+            i++;
+        }
+    }
+}
---
+++ Atividade 2 100 a 200/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 200;
+        do {
+            System.out.println("esse numero " + i);
+            i = i - 1;
+
+        }
+        while (i > 99);
+
+
+            }
+
+        }
+
